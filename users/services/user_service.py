2778058fce7d2ea4c8c950767efbaa5ea6e6eade
amir <amirjas8177@gmail.com>
2026-02-13 00:09:22 +0330

    feat: check if user exists and not save same user again

--- users/services/user_service.py
+++ users/services/user_service.py
@@ -1,13 +1,19 @@
 from datetime import datetime
+import logging
 from typing import Any
 
 from users.models import User
+
+logger = logging.getLogger("aury.users")
 
 
 class UserService:
 
     @staticmethod
     async def create_user(user_id: Any, username: str | None, first_name: str | None, last_name: str | None):
+        exists = User.find(User.telegram_id == user_id)
+        if exists:
+            return True
         user = User(
             telegram_id=user_id,
             username=username,
@@ -20,7 +26,10 @@ class UserService:
             updated_at=datetime.now(),
             last_interaction=datetime.now(),
         )
-        await user.create()
+        await user.insert()
+        logger.info("user saved=%s", user_id)
+
+        return False
 
 
 async def process_user_message(user_id: Any, text: str) -> None:
