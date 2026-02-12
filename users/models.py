e05f65cacb2c45847da216947e4211d2937e4a77
amir <amirjas8177@gmail.com>
2026-02-13 00:09:42 +0330

    fix: add unique index for telegram id and username

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from enum import unique
 from typing import Optional
 
 from beanie import Document
 from pydantic import ConfigDict, Field
+from pymongo import IndexModel
 
 
 class User(Document):
@@ -13,12 +15,17 @@ class User(Document):
     phone: Optional[str] = Field(None, description="Phone number")
     is_active: bool = Field(default=True, description="Is user active")
     is_blocked: bool = Field(default=False, description="Is user blocked")
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
     last_interaction: Optional[datetime] = Field(None, description="Last interaction with bot")
     
     class Settings:
         name = "users"
+
+        indexes = [
+            IndexModel("telegram_id", unique=True),
+            IndexModel("username", unique=True),
+        ]
 
     model_config = ConfigDict(
         json_schema_extra={
